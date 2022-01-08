30b0cae961e6aec7ade396c1aa5e1d73506074da
Irwin <rodriguez.irwin@gmail.com>
2022-01-08 23:51:23 +0100

    Add project files.

---
+++ common.h
@@ -0,0 +1,6 @@
+#ifndef clox_common_h
+#define clox_common_h
+#include <stdbool.h> // bool
+#include <stddef.h> // size_t
+#include <stdint.h> // int8_t, int16_t, int32_t, int64_t, uint8_t, uint16_t, uint32_t
+#endif
